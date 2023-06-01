6efd845df033b6d051058715fbd09b0293e798bf
Haseeb ur rehman <haseeb.ktk57@gmail.com>
2023-06-02 00:25:01 +0500

    tweet details API

--- app/tweet/[id].tsx
+++ app/tweet/[id].tsx
@@ -1,13 +1,26 @@
 import { useSearchParams } from "expo-router";
-import tweets from "../../assets/data/tweets";
 import Tweet from "../../components/Tweet";
-import { Text } from "react-native";
+import { ActivityIndicator, Text } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { getTweet } from "../../lib/api/tweets";
 
 export default function TweetScreen() {
-  const params = useSearchParams();
-  const tweet = tweets.find((tweet) => tweet.id === params.id);
-  if (!tweet) {
+  const { id } = useSearchParams();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["tweet", id],
+    queryFn: () => getTweet(id as string),
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>{error?.message}</Text>;
+  }
+  if (!data) {
     return <Text>Tweet not found!</Text>;
   }
-  return <Tweet tweet={tweet} />;
+  return <Tweet tweet={data} />;
 }
--- lib/api/tweets.ts
+++ lib/api/tweets.ts
@@ -19,3 +19,23 @@ export const listTweets = async () => {
   const data = await res.json();
   return data;
 };
+
+
+export const getTweet = async(id:string)=>{
+    const res = await fetch(`${API_URL}/tweet/${id}`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+    
+      if (res.status === 401) {
+        throw new Error("Not authorized.Please login");
+       }
+      if (res.status !== 200) {
+       throw new Error("Error fetching the tweet detail");
+      }
+    
+      const data = await res.json();
+      return data;
+
+}
